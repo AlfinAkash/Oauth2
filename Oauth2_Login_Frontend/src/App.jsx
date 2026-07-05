39c9438d3e407fc03e623afce9a5c3a7805bba08
AlfinAkash <alfinakash2001@gmail.com>
2026-07-05 21:40:27 +0530

    fix: login endpoint removed

--- Oauth2_Login_Frontend/src/App.jsx
+++ Oauth2_Login_Frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import Login from "./components/Login";
-import ProtectedRoute from "./components/ProtectedRoute";
+
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<ProtectedRoute />} />
+          <Route path="/" element={<Login />} />
+          {/* <Route path="/" element={<ProtectedRoute />} /> */}
         </Routes>
       </BrowserRouter>
     </AuthProvider>
